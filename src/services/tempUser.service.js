f3d90f68b9a03c337b9aa5b70961cbd65baf7f0e
DonggunLim <Ldonggun6766@gmail.com>
2025-01-04 14:57:56 +0900

    feat: tempUser.service layer추가

---
+++ src/services/tempUser.service.js
@@ -0,0 +1,24 @@
+const TempUser = require("../schemas/tempUser.schema");
+
+const createTempUser = async ({ email, password, otp }) => {
+  try {
+    const user = await TempUser.create({ email, password, otp });
+    return user;
+  } catch (error) {
+    throw new Error("[DB Error] createTempUser", { cause: error });
+  }
+};
+
+const findTempUserByEmail = async ({ email }) => {
+  try {
+    const user = await TempUser.findOne({ email }).lean();
+    return user;
+  } catch (error) {
+    throw new Error("[DB Error] findTempUserByEmail", { cause: error });
+  }
+};
+
+module.exports = {
+  findTempUserByEmail,
+  createTempUser,
+};
