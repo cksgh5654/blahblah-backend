4305e0e606db914a4c6660c32be36918d4e97638
DonggunLim <Ldonggun6766@gmail.com>
2025-01-05 10:58:47 +0900

    feat: cookieParser middleware 적용

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const apiController = require("./src/controllers");
 const config = require("./consts");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api", apiController);
 
