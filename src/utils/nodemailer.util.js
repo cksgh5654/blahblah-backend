28703fbc41c16bb5baa3cb45455404a811ea37db
DonggunLim <Ldonggun6766@gmail.com>
2025-01-06 03:36:35 +0900

    feat: nodemailer sendMailForResetPassword 함수추가

--- src/utils/nodemailer.util.js
+++ src/utils/nodemailer.util.js
@@ -31,6 +31,29 @@ const sendMailForSignup = async (email, otp) => {
   }
 };
 
+const sendMailForResetPassword = async (email, otp) => {
+  const mailOptions = {
+    from: config.nodemailer.authEmail,
+    to: email,
+    subject: "Blahblah 비밀번호 재설정 otp 요청입니다.",
+    html: `
+          <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background-color: #f9f9f9;">
+              <h2 style="color: #333;">${email} 비밀번호 재설정 OTP 요청</h2>
+              <p style="color: #555; font-size: 16px;">OTP를 입력하여 비밀번호 설정을 완료하세요.</p>
+              <div style="margin: 20px 0; padding: 10px; font-size: 24px; font-weight: bold; color: #fff; background-color: #007BFF; border-radius: 5px; display: inline-block;">
+              ${otp} 
+              </div>
+              <p style="color: #888; font-size: 14px;">5분간 유효합니다.</p>
+          </div>`,
+  };
+  try {
+    return await transporter.sendMail(mailOptions);
+  } catch (error) {
+    throw new Error(`[NODEMAILDER 에러] sendMailForSignup`, { cause: error });
+  }
+};
+
 module.exports = {
   sendMailForSignup,
+  sendMailForResetPassword,
 };
