8137d0bd33793822836c43da2059c6fce18925a8
DonggunLim <Ldonggun6766@gmail.com>
2025-01-14 22:56:13 +0900

    fix: 삭제되지않은 post만 가져오게 수정

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,17 +1,17 @@
-const Post = require('../schemas/post.schema');
-const Comment = require('../schemas/comment.schema');
+const Post = require("../schemas/post.schema");
+const Comment = require("../schemas/comment.schema");
 
 const createPost = async ({
   creator,
   boardId: board,
   title,
   content,
-  type = 'basic',
+  type = "basic",
 }) => {
   const post = await Post.create({ creator, board, title, content, type });
 
   if (!post) {
-    const errorMsg = '게시글 등록에 실패했습니다.';
+    const errorMsg = "게시글 등록에 실패했습니다.";
     return { errorMsg };
   }
 
@@ -23,12 +23,12 @@ const createPost = async ({
 
 const getPost = async ({ postId: _id }) => {
   const post = await Post.findOne({ _id })
-    .populate('creator', 'image nickname')
-    .populate('board', 'url')
+    .populate("creator", "image nickname")
+    .populate("board", "url")
     .lean();
 
   if (!post) {
-    const errorMsg = '게시글 조회에 실패했습니다.';
+    const errorMsg = "게시글 조회에 실패했습니다.";
     return { errorMsg };
   }
 
@@ -44,7 +44,7 @@ const updatePost = async ({ postId: _id, title, content }) => {
   const post = await Post.findByIdAndUpdate({ _id }, { title, content }).lean();
 
   if (!post) {
-    const errorMsg = '게시글을 수정에 실패했습니다.';
+    const errorMsg = "게시글을 수정에 실패했습니다.";
     return { errorMsg };
   }
 
@@ -63,7 +63,7 @@ const deletePost = async ({ postId: _id }) => {
   ).lean();
 
   if (!post) {
-    const errorMsg = '게시글 삭제에 실패했습니다.';
+    const errorMsg = "게시글 삭제에 실패했습니다.";
     return { errorMsg };
   }
 
@@ -73,7 +73,7 @@ const deletePost = async ({ postId: _id }) => {
   );
 
   if (!deletedComments) {
-    const errorMsg = '해당 게시글의 댓글들 삭제에 실패했습니다.';
+    const errorMsg = "해당 게시글의 댓글들 삭제에 실패했습니다.";
     return { errorMsg };
   }
 
@@ -87,14 +87,14 @@ const matchOwner = async ({ postId, creator }) => {
   const post = await Post.findOne({ _id: postId }).lean();
 
   if (!post) {
-    const errorMsg = '게시글을 조회에 실패했습니다.';
+    const errorMsg = "게시글을 조회에 실패했습니다.";
     return { errorMsg };
   }
 
   const isOwner = String(post.creator) === creator;
 
   if (!isOwner) {
-    const errorMsg = '해당 게시글의 수정 및 삭제 권한이 없습니다.';
+    const errorMsg = "해당 게시글의 수정 및 삭제 권한이 없습니다.";
     return { errorMsg };
   }
 
@@ -107,7 +107,7 @@ const matchOwner = async ({ postId, creator }) => {
 const getPostByBoardId = async (boardId) => {
   try {
     const posts = await Post.find({ board: boardId, deletedAt: null }) //
-      .populate('creator')
+      .populate("creator")
       .lean();
     return posts;
   } catch (error) {
@@ -126,8 +126,8 @@ const getBoardPostsCount = async (boardId) => {
 const getPostsByUserId = async (userId, { limit = 20, page }) => {
   const skip = (page - 1) * limit;
   try {
-    const posts = Post.find({ creator: userId })
-      .populate('board')
+    const posts = Post.find({ creator: userId, deletedAt: null })
+      .populate("board")
       .skip(skip)
       .limit(limit)
       .lean();
@@ -139,7 +139,10 @@ const getPostsByUserId = async (userId, { limit = 20, page }) => {
 
 const getUserPostsCount = async (userId) => {
   try {
-    return await Post.find({ creator: userId }).countDocuments();
+    return await Post.find({
+      creator: userId,
+      deletedAt: null,
+    }).countDocuments();
   } catch (error) {
     throw new Error(`[DB 에러 getUserPostsCount]`, { cause: error });
   }
